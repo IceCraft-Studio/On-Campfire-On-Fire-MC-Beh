fc440e220dc5b83629c0c6296db2de6d7b908037
PavelDobCZ23 <pavel23dob@gmail.com>
2023-03-14 21:22:56 +0100

    Fixed breaking changes of Script-API in version 1.19.70.02

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-import { system, world, BlockLocation } from '@minecraft/server';
+import { system, world } from '@minecraft/server';
 import { SCHEDULED_TICKS } from './config/config';
 const DIMENSION_LIST = ['minecraft:overworld','minecraft:nether','minecraft:the_end'];
 
-system.runSchedule(() => {
+system.runInterval(() => {
     for (let index = 0;index < DIMENSION_LIST.length;index++) {
         const dimension = world.getDimension(DIMENSION_LIST[index]);
         for (const entity of dimension.getEntities()) {
             try {
-                const blockLocation = new BlockLocation(Math.floor(entity.location.x),Math.floor(entity.location.y),Math.floor(entity.location.z));
+                const blockLocation = {x:Math.floor(entity.location.x),y:Math.floor(entity.location.y),z:Math.floor(entity.location.z)};
                 const blockStandingIn = dimension.getBlock(blockLocation);
                 if (blockStandingIn?.typeId === 'minecraft:campfire' || blockStandingIn?.typeId === 'minecraft:soul_campfire') {
                     entity.setOnFire(8);
